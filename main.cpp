a91f4978571daf49035f52a5e4905bc7dba6dc2a
kylejow <77653395+kylejow@users.noreply.github.com>
2022-07-10 14:19:29 -0700

    Added main loop

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 using std::cout;
+using std::endl;
+using std::cin;
 using std::vector;
 #define SizeDisplay 50
 
@@ -22,5 +24,27 @@ int main(){
         Display.push_back(line);
     }
     printDisplay(Display);
+
+    std::string input;
+    while(1){
+        system("cls");
+        cout << "1. Selection Sort\n"
+             << "2. Insertion Sort\n"
+             << "3. Merge Sort\n"
+             << "4. Quick Sort\n"
+             << "\n\nq to exit" << endl;
+        cin >> input;
+        if(input == "1"){
+            system ("cls");
+        }else if(input == "2"){
+            system ("cls");
+        }else if(input == "3"){
+            system ("cls");
+        }else if(input == "4"){
+            system ("cls");
+        }else if(input == "q")
+         break;
+    }
+    system ("cls");
     return 0;
 }
